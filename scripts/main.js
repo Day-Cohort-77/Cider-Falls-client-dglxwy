8e6b200cf91456d823d6dc08aec112dec2edebcb
Gary Venus <garyjdougherty@gmail.com>
2025-06-09 13:28:13 -0500

    last problem code

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 import { guestList } from "./guestList.js";
 import { areasList } from "./areaList.js";
+import { servicesList } from "./servicesList.js";
 
 const mainAreaGrid = document.querySelector("#area");
 const mainGuests = document.querySelector("#guest");
+const mainServices = document.querySelector("#services");
 const areaHTML = `
 <h2>Areas</h2>
 <article class="details">
@@ -14,8 +16,17 @@ const guestHtml = `
         <h3>Guests</h2>
         ${guestList()}
     </section>`;
+const serviceHTML = `
+<h4>Services</h4>
+<article class="details">
+    <section class="servicesList">
+        ${servicesList()}
+    </section>
+</article>`
 
 
 mainAreaGrid.innerHTML = areaHTML;
 mainGuests.innerHTML = guestHtml;
+mainServices.innerHTML = serviceHTML;
+
 
--- scripts/servicesList.js
+++ scripts/servicesList.js
@@ -1,16 +1,18 @@
-import { getServices } from "./database,js";
+import { getServices } from "./database.js";
+import { getAreas } from "./database.js";
 
 export const servicesList = () => {
   const services = getServices();
   let servicesHTML = "<ul>";
 
   for (const service of services) {
-    `<li 
+    servicesHTML += `<li 
             data-id = "${service.id}"
             data-type = "service"
             data-areaId = "${service.areaId}"
+            data-service = "${service.type}"
             >
-                ${service.name}
+                ${service.type}
         </li>`;
   }
 
@@ -18,3 +20,29 @@ export const servicesList = () => {
 
   return servicesHTML;
 };
+
+document.addEventListener("click", (event) => {
+  if (event.target.dataset.type === "service") {
+    const serviceType = event.target.dataset.service;
+    let areas = getAreas();
+    let areaService = [];
+    for (const area of areas) {
+      if (area.id === parseInt(event.target.dataset.areaId))
+        
+        areaService.push(area.name);
+      if (areaService.length > 0;) {
+        window.alert(`${serviceType} is available in: ${areaService.join(', ')}`);
+        //window.alert(`${serviceType} is available in ${area.name}`)
+
+      } else {
+        window.alert(`${serviceType} is available in ${area.name}`);
+      }
+    break;
+    }
+
+  }
+}
+)
+
+//FOR (LET I=O: I < AREA.ID; I++)
+//Array.PUSH(AREA)
